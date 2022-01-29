0d1acfafb647d88445954649108feb42432566c8
Håvard <havard.skjerstein@gmail.com>
2022-01-29 21:28:44 +0100

    Todo commit

--- app/screens/Todo.js
+++ app/screens/Todo.js
@@ -2,12 +2,35 @@ import React, {useState} from "react";
 import {StyleSheet, TouchableOpacity, Text, View, Image, Button, TextInput} from "react-native"; 
 
 function ToDo(props){
+    const [enteredGoals, setEnteredGoal] = useState(""); 
+    const [courseGoals, setCourseGoals] = useState([]); 
+
+    const goalInputHandler = (enteredText) =>{
+        setEnteredGoal(enteredText)
+    };
+
+    const addGoalHandler = () => {
+        setCourseGoals(currentGoals => [...currentGoals, enteredGoals]); 
+    }
+
     return(
         /* Background*/ 
         <View style={styles.container}>
+            <View>
+                <Text style ={styles.todo}> Todo: </Text>
+            </View>
+
             <View style={styles.inputContainer}>
-                <TextInput placeholder = "Goal" style ={styles.input}/>
-                <Button title = "Add" />
+                <TextInput 
+                placeholder = "Goal" 
+                style ={styles.input}
+                onChangeText={goalInputHandler}
+                value={enteredGoals}
+                />
+                <Button title ="Add" onPress={addGoalHandler} />
+            </View>
+            <View>
+                {courseGoals.map((goal) => <text>{goal}</text>)}
             </View>
         </View>
     ); 
@@ -17,18 +40,22 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: "#88d498",
-        alignItems: 'center',
-        justifyContent: "flex-start",
         padding: 50, 
     },
 
     inputContainer:{
+        flexDirection: "row",
+        justifyContent: "space-between",
         borderColor: "black", 
-        borderWidth: 1, 
+        borderWidth: 2, 
         padding: 10, 
         width: 300,
         height: 60, 
-    }
+    },
+
+    todo: {
+        fontSize: 24,
+    }, 
 }); 
 
 export default ToDo; 
