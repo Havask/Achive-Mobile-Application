a91d2b2dd1b6d560eaa802c47366e22d09cbb1db
Havard <haask333@gmail.com>
2022-02-15 16:12:30 +0100

    asd

---
+++ README.md
@@ -0,0 +1,11 @@
+Include A README file that contains
+-A brief description of the project
+-Installation instructions
+-A short example/tutorial
+
+
+
+Write an API documentation
+-What a function do
+-What the function's parameters or arguments are
+-What a function returns
--- api/firebaseApi.js
+++ api/firebaseApi.js
@@ -1,5 +1,19 @@
-import { Firestore } from "firebase/firestore/lite";
+import { Firestore, getDoc, orderBy } from "firebase/firestore/lite";
 import firebase from "../react-native-firebase"; 
+
+import {collection, 
+        addDoc,
+        doc,
+        setDoc, 
+        addDoc, 
+        getDoc, 
+        onSnapshot, 
+        query, 
+        where, 
+        getDocs, 
+        orderBy, 
+        limit, 
+     } from "firebase/firestore"; 
 
 //adds a new task to the backend
 export function addTask(task, addComplete){
@@ -37,4 +51,64 @@ export function removeGroup(){
 }
 export function addGroup(){
     return; 
+}
+
+
+
+//write to a document
+export function writeToDoc(){
+    const docData={
+        task: "Rydd kjøkkenet", 
+        description: "To the dishes", 
+        status: "Ikke gjort",
+    }; 
+
+    //replaces the document
+    setDoc(writeToDoc, docData, {merge:true})
+    .then(() => {
+        consol.log("Written to database")
+    })
+    .catch((error) => {
+        console.log("error accured ${error}"); 
+    }); 
+}
+
+
+const ordersCollection = collection(Firestore, "groups")
+//makes a document in the collection with a random generated ID
+export async function addNewDocument(){
+    const newDoc = await addDoc(ordersCollection, {
+        task: "Rydd kjøkkenet", 
+        description: "To the dishes", 
+        status: "Ikke gjort",
+    }); 
+    console.log("new document created at ${newDoc.path}");
+}
+
+//reads a single document
+export async function readASingleDoc(){
+    const mySnapshot = await getDoc(tasks);
+    if(mySnapshot.exists){
+        const docData = mySnapshot.data(); 
+        console.log("My data is ${json.stringify(docData)}"); 
+    }
+}
+
+//function for grabbing multible documents at once (query)
+export function queryForDocuments(){
+    const TaskQuery = query(
+        collection(Firestore, "Groups"), 
+        //filtering the query
+        where("status" == "aktiv"),
+        orderBy("Status"),
+        //limits the results. Kan bruke dette til å 
+        //spørre databasen for ting å gjøre også og displaye et fixed antall 
+        //det gjør det også billigere
+        limit(10), 
+    ); 
+    //returns an array of the results from query
+    const querySnapshot = await getDocs(TaskQuery); 
+    querySnapshot.forEach((snap) => {
+        console.log("Document ${snap.id} contains ${JSON.stringify(snap.data())}")
+    })
 }
